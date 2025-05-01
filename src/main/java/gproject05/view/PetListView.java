db49f20a8489dd20b3b4df28ef364d0ae54c60ba
ryan4336 <rahhhp@umsystem.edu>
2025-05-01 11:42:56 -0500

    added all necessary buttons (not functional yet)

--- src/main/java/gproject05/view/PetListView.java
+++ src/main/java/gproject05/view/PetListView.java
@@ -9,8 +9,14 @@ import gproject05.pets.Pet;
 public class PetListView extends JFrame {
 	private JPanel panel;
 	private JList<Pet> list;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+	private JButton btnNewButton_4;
+	private JTextField txtSortBy;
 	
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public PetListView() {
 		setTitle("Pet List Page");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -21,7 +27,7 @@ public class PetListView extends JFrame {
 		getContentPane().setLayout(null);
 		
 		JButton btnNewButton = new JButton("Add Pet");
-		btnNewButton.setBounds(6, 337, 100, 29);
+		btnNewButton.setBounds(6, 306, 100, 29);
 		panel.add(btnNewButton);
 		
 		DefaultListModel<Pet> modelList = new DefaultListModel<Pet>();
@@ -29,6 +35,33 @@ public class PetListView extends JFrame {
 		
 		list.setBounds(6, 6, 488, 288);
 		panel.add(list);
+		
+		btnNewButton_1 = new JButton("Remove Pet");
+		btnNewButton_1.setBounds(6, 337, 100, 29);
+		panel.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("Adopt Pet");
+		btnNewButton_2.setBounds(118, 337, 92, 29);
+		panel.add(btnNewButton_2);
+		
+		btnNewButton_3 = new JButton("View Details");
+		btnNewButton_3.setBounds(222, 337, 108, 29);
+		panel.add(btnNewButton_3);
+		
+		btnNewButton_4 = new JButton("Save");
+		btnNewButton_4.setBounds(377, 337, 117, 29);
+		panel.add(btnNewButton_4);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Name", "Species", "Age"}));
+		comboBox.setBounds(377, 307, 105, 27);
+		panel.add(comboBox);
+		
+		txtSortBy = new JTextField();
+		txtSortBy.setText("Sort by:");
+		txtSortBy.setBounds(317, 306, 64, 26);
+		panel.add(txtSortBy);
+		txtSortBy.setColumns(10);
 	}
 
 
@@ -36,6 +69,4 @@ public class PetListView extends JFrame {
 	public DefaultListModel<Pet> getList() {
 		return (DefaultListModel<Pet>) list.getModel();
 	}
-	
-	
 }
